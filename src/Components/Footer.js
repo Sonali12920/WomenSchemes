d474f16bf7c98eff7ccfed14c0b3b954c4f8ed1c
nivedita0604 <niveditakumari0604@gmail.com>
2021-11-18 19:47:39 +0530

    added basic footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import "../styles/footer.css";
 
 export default function Footer() {
-    return (
-        <div>
-            Footer
+  return (
+    <div className="main-Footer">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <ul className="list-unstyled">
+              <li>
+                <a className="nav-link" href="/scholarships">
+                  Scholarships
+                </a>
+              </li>
+              <li>
+                <a className="nav-link" href="/internships">
+                  Internships
+                </a>
+              </li>
+              <li>
+                <a className="nav-link" href="/fellowships">
+                  Fellowships
+                </a>
+              </li>
+              <li>
+                <a className="nav-link" href="/research">
+                  Research
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-    )
+        <hr />
+        <div className="row">
+          <p className="col-sm">
+            &copy;{new Date().getFullYear()} Web Development Team | All rights
+            reserved | Terms of Service | Privacy
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
