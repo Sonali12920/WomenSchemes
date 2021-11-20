78a06217ce15ca3e8aa76ce122dd7f7048c11780
nivedita0604 <niveditakumari0604@gmail.com>
2021-11-20 17:26:41 +0530

    db date formatting

--- src/Components/Research.js
+++ src/Components/Research.js
@@ -1,51 +1,59 @@
 import React from "react";
 import db from "../database/fakedb.json";
-import { Card, Button, CardGroup } from "react-bootstrap";
+import { Card, Button, CardGroup, Row, Col } from "react-bootstrap";
+import Filters from "./Filters";
 // import "../styles/Card.css";
 
 function comparedates(date) {
-    var today = new Date();
-    const date1 = new Date(date);
-    const date2 = new Date(today);
-    const diffTime = Math.abs(date2 - date1);
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    return diffDays;
-  }
-  
+  var today = new Date();
+  const date1 = new Date(date);
+  const date2 = new Date(today);
+  console.log(date1);
+  /*console.log(date2);*/
+  const diffTime = Math.abs(date2 - date1);
+
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  return diffDays;
+}
 
 export default function Research() {
   return (
     <div>
       <h1 style={{ textAlign: "center", margin: "4px" }}>Research</h1>
-      <div>
-        <CardGroup>
-          {db.map((el) => {
-            return (
-              <div className="col-sm-4">
-                <Card body key={el.id}>
-                  <h4>{el.name}</h4>
-                  <p>{el.desc}</p>
-                  <p>
-                    <span>
-                      <b>Apply date:</b>
-                    </span>
-                    {el["apply-date"]}
-                  </p>
-                  <p>
-                    <span>
-                      <b>Available: </b>
-                    </span>
-                    {comparedates(el["end-date"])} days to go
-                  </p>
-                  <Button class="btn btn-primary" href={el.link}>
-                    Link
-                  </Button>
-                </Card>
-              </div>
-            );
-          })}
-        </CardGroup>
-      </div>
+      <Row>
+        <Col md={2}>
+          <Filters />
+        </Col>
+        <Col md={10}>
+          <CardGroup>
+            {db.map((el) => {
+              return (
+                <div className="col-sm-4">
+                  <Card body key={el.id}>
+                    <h4>{el.name}</h4>
+                    <p>{el.desc}</p>
+                    <p>
+                      <span>
+                        <b>Apply date:</b>
+                      </span>
+                      {el["apply-date"]}
+                    </p>
+                    <p>
+                      <span>
+                        <b>Available: </b>
+                      </span>
+                      {comparedates(el["end-date"])} days to go
+                    </p>
+                    <Button class="btn btn-primary" href={el.link}>
+                      Link
+                    </Button>
+                  </Card>
+                </div>
+              );
+            })}
+          </CardGroup>
+        </Col>
+      </Row>
     </div>
   );
 }
