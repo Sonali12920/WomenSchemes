b3f2a840908a57f4258f887d6f47de31c00d30a0
nivedita0604 <niveditakumari0604@gmail.com>
2021-11-24 19:44:53 +0530

    card comp usage

--- src/Components/Research.js
+++ src/Components/Research.js
@@ -1,20 +1,9 @@
 import React from "react";
 import db from "../database/fakedb.json";
-import { Card, Button, CardGroup, Row, Col } from "react-bootstrap";
+import { CardGroup, Row, Col } from "react-bootstrap";
+import CardsComp from "./CardComponent";
 import Filters from "./Filters";
 // import "../styles/Card.css";
-
-function comparedates(date) {
-  var today = new Date();
-  const date1 = new Date(date);
-  const date2 = new Date(today);
-  console.log(date1);
-  /*console.log(date2);*/
-  const diffTime = Math.abs(date2 - date1);
-
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-  return diffDays;
-}
 
 export default function Research() {
   return (
@@ -27,29 +16,7 @@ export default function Research() {
         <Col md={10}>
           <CardGroup>
             {db.map((el) => {
-              return (
-                <div className="col-sm-4">
-                  <Card body key={el.id}>
-                    <h4>{el.name}</h4>
-                    <p>{el.desc}</p>
-                    <p>
-                      <span>
-                        <b>Apply date:</b>
-                      </span>
-                      {el["apply_date"]}
-                    </p>
-                    <p>
-                      <span>
-                        <b>Available: </b>
-                      </span>
-                      {comparedates(el["end_date"])} days to go
-                    </p>
-                    <Button class="btn btn-primary" href={el.link}>
-                      Link
-                    </Button>
-                  </Card>
-                </div>
-              );
+              return <>{CardsComp(el)}</>;
             })}
           </CardGroup>
         </Col>
