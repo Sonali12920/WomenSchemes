f68295b7042402c9db8d0f8b05b131c4ed032b5b
sonali <sona120920@gmail.com>
2021-11-28 20:19:13 +0530

    chnages in navbar routes

--- src/Components/NavbarComponent.js
+++ src/Components/NavbarComponent.js
@@ -21,7 +21,7 @@ export default function NavbarComponent() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link as={Link} to={"/home"}>
+                <Nav.Link as={Link} to={"/"}>
                   Home
                 </Nav.Link>
                 <Nav.Link as={Link} to={"/scholarships"}>
@@ -61,7 +61,7 @@ export default function NavbarComponent() {
         </div>
         <div>
           <Routes>
-            <Route exact path="/home" element={<Home />} />
+            <Route exact path="/" element={<Home />} />
             <Route exact path="/scholarships" element={<Scholarships />} />
             <Route exact path="/fellowships" element={<Fellowships />} />
             <Route exact path="/internships" element={<Internships />} />
