e8ce7736d9551a797fbf192aa9108700d92a5433
nivedita0604 <niveditakumari0604@gmail.com>
2021-12-04 17:53:55 +0530

    home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,58 @@
 import React from "react";
 import Header from "./Header";
 import "../App.css";
+import "../styles/Home.css";
+import { Row, Col, Button } from "react-bootstrap";
 
 export default function Home() {
   return (
     <div>
       <div class="container-fluid">
         <Header />
-        <div class="row">
-          <div class="col-3"></div>
+        <div>
+          <Row>
+            <h3 className="text-center font-monospace">I'm Looking for</h3>
+            <Col sm={3}>
+              <Button
+                class="btn"
+                size="sm"
+                variant="outline-dark"
+                href="./WomenSchemes/scholarships"
+              >
+                Scholarships
+              </Button>
+            </Col>
+            <Col sm={3}>
+              <Button
+                class="btn"
+                size="sm"
+                variant="outline-dark"
+                href="./WomenSchemes/internships"
+              >
+                Internships
+              </Button>
+            </Col>
+            <Col sm={3}>
+              <Button
+                class="btn"
+                size="sm"
+                variant="outline-dark"
+                href="./WomenSchemes/fellowships"
+              >
+                Fellowships
+              </Button>
+            </Col>
+            <Col sm={3}>
+              <Button
+                class="btn"
+                size="sm"
+                variant="outline-dark"
+                href="./WomenSchemes/research"
+              >
+                Research
+              </Button>
+            </Col>
+          </Row>
         </div>
       </div>
     </div>
